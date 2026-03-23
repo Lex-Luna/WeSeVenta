b1a0b8918b162ffd7c9cde08f2c13014dc87881c
Alexis Luna <alexisgabriel.lu@gmail.com>
2026-03-23 12:51:36 +0100

    creacion del proyecto

---
+++ src/main/java/com/lexcorp/apirest/apirst_java/ApirstJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lexcorp.apirest.apirst_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirstJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirstJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexcorp/apirest/apirst_java/ApirstJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexcorp.apirest.apirst_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirstJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
